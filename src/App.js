9ca3c11ae1f0099eded33212351bde1fc9a62b88
Kurt Pachinger <48163461+KurtPachinger@users.noreply.github.com>
2022-09-05 16:49:37 -0400

    tweak tolerances

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import * as THREE from 'three'
 //threejs.org/examples/?q=modifi#webgl_modifier_subdivision
 //threejs.org/examples/?q=simp#webgl_modifier_simplifier
 //three/examples/jsm/materials/MeshGouraudMaterial.js
-import { useRef, useState, useMemo } from 'react'
+import { useRef, useEffect, useState, useMemo } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls, MeshReflectorMaterial, AdaptiveDpr } from '@react-three/drei'
 import { Brush, Subtraction, Addition } from '@react-three/csg'
@@ -14,11 +14,11 @@ export default function App(props) {
 
   const R = vlay.v.R * 4
   return (
-    // frameloop="demand" / invalidate
     <Canvas frameloop="demand" performance={{ min: 0.1 }} shadows camera={{ position: [0, R, R] }} onCreated={(state) => vlay.init(state)}>
       <OrbitControls makeDefault />
-      <pointLight intensity={6} position={[0, R, R * 2]} castShadow />
-      <pointLight intensity={2} position={[0, R / 4, 0]} castShadow />
+      <pointLight name="top" intensity={6} position={[0, R, R * 2]} castShadow />
+      <pointLight name="mid" intensity={2} position={[0, R / 4, 0]} castShadow />
+      <pointLight name="low" intensity={1} position={[0, -R, -R * 2]} />
       <gridHelper args={[R * 2, 4]} position={0} />
       <axesHelper args={[R / 2]} />
       <group name="out" ref={vlay.v.out}>
@@ -39,7 +39,7 @@ export default function App(props) {
           metalness={0.6}
         />
       </mesh>
-      <AdaptiveDpr />
+      <AdaptiveDpr pixelated />
     </Canvas>
   )
 }
@@ -64,8 +64,10 @@ function CSG(props) {
     }
   })
 
-  const geo = new THREE.IcosahedronBufferGeometry(vlay.v.R * 2, 3)
   const neg = new THREE.PlaneBufferGeometry(0, 0)
+  const geo = new THREE.IcosahedronBufferGeometry(vlay.v.R * 2, 3)
+  geo.userData.geo = geo.clone()
+
   return (
     <Subtraction useGroups>
       <Subtraction a useGroups>
@@ -73,7 +75,7 @@ function CSG(props) {
         <Brush b ref={vlay.v.csg.neg} geometry={neg} material={vlay.mat.neg} />
       </Subtraction>
       <Brush b>
-        <icosahedronBufferGeometry args={[vlay.v.R / 2, 1]} />
+        <icosahedronBufferGeometry args={[vlay.v.R, 1]} />
       </Brush>
     </Subtraction>
     //<mesh name="pos" ref={vlay.v.csg.pos} material={vlay.mat.map} />
