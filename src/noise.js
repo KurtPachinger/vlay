c811a656c8554cbc6efbdf958152a770e8233263
Kurt Pachinger <48163461+KurtPachinger@users.noreply.github.com>
2022-09-21 14:39:52 -0400

    noise maintains special features
    - preserves special features
    - returns array

--- src/noise.js
+++ src/noise.js
@@ -1,12 +1,15 @@
-const noise = function (canvas, seed, uid, hist) {
-  // source
-  const MAX = canvas.width
-  let context = canvas.getContext('2d')
-  context.fillStyle = '#000000'
-  context.fillRect(0, 0, canvas.width, canvas.height)
+const noise = function (POW, qty, seed, uid, hist) {
+  // params
+  const min = 4
+  let k = {
+    inc: min,
+    pos: 0,
+    neg: 0,
+    arr: []
+  }
 
   function gen(uid, uei = 1) {
-    // uid from seed (from last or root)
+    // uid from seed (last or root)
     let S = hist[uid]
     S = S ? S ** 1.5 : ((Math.PI - 3) * 5e11) / seed
     S = Number((S * uei).toFixed().slice(-8))
@@ -15,60 +18,88 @@ const noise = function (canvas, seed, uid, hist) {
     return S
   }
 
-  // params
-  let k = {
-    d: MAX / 2,
-    pos: 0,
-    neg: 0
-  }
+  function quad(pair, incr, major) {
+    const inc = POW / incr
+    const spec = pair.major.getContext('2d')
+    const base = pair.minor.getContext('2d')
 
-  function quad(k) {
-    let d = MAX / k.d
-    let cell = false
-
-    for (let x = 0; x < canvas.width; x += d) {
-      for (let y = 0; y < canvas.height; y += d) {
+    for (let x = 0; x < POW; x += inc) {
+      for (let y = 0; y < POW; y += inc) {
         // quad
-        k.gen = gen(uid)
-        k.label = k.gen > '55555555' ? 'pos' : 'neg'
+        const color = gen(uid)
+        k.label = color > '55555555' ? 'pos' : 'neg'
+        let fill = String(color)
+        let dst
 
-        if (d === MAX / 4 && cell) {
-          // major
-          let detail = String(k.gen)
-          if (k.label === 'pos') {
-            detail = '80ff8080'
+        if (major) {
+          dst = spec
+          // absolute
+          let cell = color % 2 === 0
+          if (cell) {
+            if (k.label === 'pos') {
+              fill = '80ff8080'
+            } else {
+              fill = '10104080'
+            }
           } else {
-            detail = '10104080'
+            continue
           }
-          k.fill = detail
         } else {
-          //minor
-          let base = String(k.gen)
-          let RGB = k.label === 'pos' ? base.substring(2, 4) : base.substring(4, 6)
-          base = base.replace(RGB, '80')
+          dst = base
+          // relative
+          let RGB = k.label === 'pos' ? fill.substring(2, 4) : fill.substring(4, 6)
+          fill = fill.replace(RGB, '80')
           let A = k.label === 'pos' ? '40' : '20'
-          base = base.replace(base.substring(6, 8), A)
-          // out
-          k.fill = base
+          fill = fill.replace(fill.substring(6, 8), A)
         }
         k[k.label]++
 
-        // output
-        context.fillStyle = '#' + k.fill
-        context.fillRect(x, y, d, d)
-        cell = k.gen % 2 === 0
+        // draw
+        dst.fillStyle = '#' + fill
+        dst.fillRect(x, y, inc, inc)
       }
     }
 
     // iterate
-    if (k.d > 4) {
-      k.d /= 2
-      quad(k)
-    } else {
-      console.log(k)
+    if (!major && incr < POW / 2) {
+      incr *= 2
+      quad(pair, incr)
     }
   }
-  quad(k)
+
+  // tiles increment detail from seed until POW
+  for (let i = 0; i < qty; i++) {
+    // special
+    let major = document.createElement('canvas')
+    major.width = major.height = POW
+    // generic
+    let minor = document.createElement('canvas')
+    minor.width = minor.height = POW
+    // generic base color
+    let base = minor.getContext('2d')
+    base.fillStyle = '#000000'
+    base.fillRect(0, 0, POW, POW)
+
+    // major: while seed origin
+    k.arr[i] = { major: major, minor: minor }
+    quad(k.arr[i], k.inc, true)
+  }
+
+  for (let i = 0; i < qty; i++) {
+    // minor: until POW
+    quad(k.arr[i], k.inc)
+  }
+
+  // output
+  console.log('noise', k)
+  let composite = []
+  for (let i = 0; i < k.arr.length; i++) {
+    let pair = k.arr[i]
+    let base = pair.minor.getContext('2d')
+    base.drawImage(pair.major, 0, 0, POW, POW)
+    composite.push(pair.minor)
+  }
+  return composite
 }
 
 export { noise }
