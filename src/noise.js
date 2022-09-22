d37435f702bde2a91a5fd23ac632c6b516bc61c5
Kurt Pachinger <48163461+KurtPachinger@users.noreply.github.com>
2022-09-22 10:47:15 -0400

    re-structure

--- src/noise.js
+++ src/noise.js
@@ -1,33 +1,37 @@
-const noise = function (POW, qty, seed, uid, hist) {
+const seedmap = function (seed, size = 64, qty = 1) {
   // params
-  const min = 4
   let k = {
-    inc: min,
-    pos: 0,
-    neg: 0,
-    arr: []
+    map: [],
+    max: size / 4,
+    min: Math.max(4, size / 128),
+    dat: { pos: 0, neg: 0 }
   }
 
-  function gen(uid, uei = 1) {
+  function gen(seed, uei = 1) {
     // uid from seed (last or root)
-    let S = hist[uid]
+    let S = k.seed
     S = S ? S ** 1.5 : ((Math.PI - 3) * 5e11) / seed
     S = Number((S * uei).toFixed().slice(-8))
     // output
-    hist[uid] = S
+    k.seed = S
     return S
   }
 
-  function quad(pair, incr, major) {
-    const inc = POW / incr
+  function quad(pair, inc, major) {
     const spec = pair.major.getContext('2d')
     const base = pair.minor.getContext('2d')
 
-    for (let x = 0; x < POW; x += inc) {
-      for (let y = 0; y < POW; y += inc) {
+    for (let x = 0; x < size; x += inc) {
+      for (let y = 0; y < size; y += inc) {
+        // TO-DO: instead of linear continuous col/row,
+        // ...project to a cube face and iterate for a while
+        // ...i.e. scale, difference, position
+        // TO-DO: helper transforms matrix from one map face to another
+        // ... pos, heading, etc.
+
         // quad
-        const color = gen(uid)
-        k.label = color > '55555555' ? 'pos' : 'neg'
+        const color = gen(seed)
+        const label = color > '55555555' ? 'pos' : 'neg'
         let fill = String(color)
         let dst
 
@@ -36,7 +40,7 @@ const noise = function (POW, qty, seed, uid, hist) {
           // absolute
           let cell = color % 2 === 0
           if (cell) {
-            if (k.label === 'pos') {
+            if (label === 'pos') {
               fill = '80ff8080'
             } else {
               fill = '10104080'
@@ -47,12 +51,12 @@ const noise = function (POW, qty, seed, uid, hist) {
         } else {
           dst = base
           // relative
-          let RGB = k.label === 'pos' ? fill.substring(2, 4) : fill.substring(4, 6)
+          let RGB = label === 'pos' ? fill.substring(2, 4) : fill.substring(4, 6)
           fill = fill.replace(RGB, '80')
-          let A = k.label === 'pos' ? '40' : '20'
+          let A = label === 'pos' ? '40' : '20'
           fill = fill.replace(fill.substring(6, 8), A)
         }
-        k[k.label]++
+        k.dat[label]++
 
         // draw
         dst.fillStyle = '#' + fill
@@ -61,45 +65,45 @@ const noise = function (POW, qty, seed, uid, hist) {
     }
 
     // iterate
-    if (!major && incr < POW / 2) {
-      incr *= 2
-      quad(pair, incr)
+    if (!major && inc > k.min) {
+      inc /= 2
+      quad(pair, inc)
     }
   }
 
-  // tiles increment detail from seed until POW
+  // tiles increment detail from seed until size
   for (let i = 0; i < qty; i++) {
     // special
     let major = document.createElement('canvas')
-    major.width = major.height = POW
+    major.width = major.height = size
     // generic
     let minor = document.createElement('canvas')
-    minor.width = minor.height = POW
+    minor.width = minor.height = size
     // generic base color
     let base = minor.getContext('2d')
     base.fillStyle = '#000000'
-    base.fillRect(0, 0, POW, POW)
+    base.fillRect(0, 0, size, size)
 
     // major: while seed origin
-    k.arr[i] = { major: major, minor: minor }
-    quad(k.arr[i], k.inc, true)
+    k.map[i] = { major: major, minor: minor }
+    quad(k.map[i], k.max, true)
   }
 
   for (let i = 0; i < qty; i++) {
-    // minor: until POW
-    quad(k.arr[i], k.inc)
+    // minor: until size
+    quad(k.map[i], k.max)
   }
 
   // output
-  console.log('noise', k)
-  let composite = []
-  for (let i = 0; i < k.arr.length; i++) {
-    let pair = k.arr[i]
+  // TO-DO: re-order from (px,nx,py,ny,pz,nz) => (py,pz,ny,nz,nx,px)...?
+  for (let i = 0; i < k.map.length; i++) {
+    let pair = k.map[i]
     let base = pair.minor.getContext('2d')
-    base.drawImage(pair.major, 0, 0, POW, POW)
-    composite.push(pair.minor)
+    base.drawImage(pair.major, 0, 0, size, size)
+    k.map[i] = pair.minor
   }
-  return composite
+  console.log('noise', k)
+  return k
 }
 
-export { noise }
+export { seedmap }
